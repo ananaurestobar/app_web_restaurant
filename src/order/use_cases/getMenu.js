78cd5ed0b55cc87b7dec5e1a8191936867196906
ananaurestobar <ananaurestobar@gmail.com>
2023-01-17 17:29:45 -0500

    instacion cors incial

--- src/order/use_cases/getMenu.js
+++ src/order/use_cases/getMenu.js
@@ -1,5 +1,5 @@
 export async function getMenu() {
-  const menu =[
+  /*const menu =[
     {id:1, name:'Te Piteado Maracuya', price: 5.00},
     {id:2, name:'Alita BBQ limo', price: 14.00},
     {id:3, name:'Añañau', price: 13.00},
@@ -8,11 +8,17 @@ export async function getMenu() {
     {id:6, name:'Te Piteado Piña', price: 16.00},
     {id:7, name:'Salchipapa Clasica', price: 17.00},
     {id:8, name:'Salchipapa 4 Suyos', price: 18.00},
-  ]
+  ]*/
 
 
 
   return new Promise ((resolve, reject)=>{
-    setTimeout(()=>{return resolve(menu) }, 2000)
+    //setTimeout(()=>{return resolve(menu) }, 2000)
+     fetch(`http://localhost:3000/menu`)
+      .then((response) => resolve(response.json()))
+      .catch((err) => {
+        console.log(err.message);
+        reject(err)
+      });
   })
 }
